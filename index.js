e59b2b912079db1b107e324d5de30e9df91730fc
Ronaldo Kipkirui <ronaldochirchir8@gmail.com>
2024-12-02 09:57:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1;
+let num3 = 14;
+let num4 = 10;
+let mod = num3 % num4;
+const max = Math.max(10, 15, 20, 5);
 
-// Write your code here
+
+
+
